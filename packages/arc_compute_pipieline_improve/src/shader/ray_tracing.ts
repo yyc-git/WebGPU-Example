604251243f41f178f02ef2feefe28e321e743329
yyc-git <395976266@qq.com>
2022-08-15 11:51:41 +0800

    refactor(arc_compute_pipieline_improve): clean ray_tracing.ts code

--- packages/arc_compute_pipieline_improve/src/shader/ray_tracing.ts
+++ packages/arc_compute_pipieline_improve/src/shader/ray_tracing.ts
@@ -30,19 +30,13 @@ struct AABB2D {
 // const maxDepth = 20;
 // const workGroupSize = 64;
 
-// var<workgroup>rayPacketRingIntersects: array<RingIntersect, 64>;
 var<workgroup>stackContainer: array<TopLevel, 20>;
-// var<workgroup>stackSize: u32;
 var<workgroup>bvhNodeFirstActiveRayIndexs: array <u32, 20>;
 var<workgroup>rayPacketAABB: AABB2D;
 var<workgroup>isRayPacketAABBIntersectWithTopLevelNode: bool;
-// var<workgroup>rayPacketPointInScreenForFindMin: array<vec2<f32>, 64>;
-// var<workgroup>rayPacketPointInScreenForFindMax: array<vec2<f32>, 64>;
 var<workgroup>rayPacketTempForFindFirstActiveRayIndex: array<bool, 64>;
 var<workgroup>rayPacketTemp2ForFindFirstActiveRayIndex: u32;
 var<workgroup>rayPacketRingIntersectLayer: array<u32, 64>;
-// var<workgroup>rayPacketHasNotIntersectRay: bool;
-// var<workgroup>rayPacketRingIntersectMinLayer: u32;
 var<workgroup>isNodeBehindRayPacket: bool;
 
 
@@ -128,19 +122,7 @@ struct Material {
 
 @binding(6) @group(0) var<uniform> screenDimension : ScreenDimension;
 
-// fn _isIntersectWithAABB2D(ray: Ray, aabb: AABB2D) -> bool {
-//   var rayTarget = ray.rayTarget;
-//   var screenMin = aabb.screenMin;
-//   var screenMax = aabb.screenMax;
-
-// return rayTarget.x > screenMin.x && rayTarget.x < screenMax.x && rayTarget.y > screenMin.y && rayTarget.y < screenMax.y;
-// }
-
-
-// fn _isIntersectWithRing(ray: Ray, instance: Instance, geometry: Geometry) -> bool {
 fn _isIntersectWithRing(pointInScreen: vec2<f32>, instance: Instance, geometry: Geometry) -> bool {
-  // var rayTarget = ray.rayTarget;
-
 var localPosition = instance.localPosition;
 
   var c = geometry.c;
@@ -178,15 +160,6 @@ fn _getLeafInstanceCount(leafInstanceCountAndMaxLayer: u32) -> u32 {
   return (leafInstanceCountAndMaxLayer >> 8) & 0xffffff;
 }
 
-// fn _buildRayPacketAABB (firstActiveRayIndex:u32,pointInScreen: vec2<f32>, LocalInvocationIndex : u32) {
-// if(LocalInvocationIndex>= firstActiveRayIndex){
-//     rayPacketAABB.screenMin = vec2<f32>(min(rayPacketAABB.screenMin.x, pointInScreen.x), min(rayPacketAABB.screenMin.y, pointInScreen.y));
-//     rayPacketAABB.screenMax = vec2<f32>(max(rayPacketAABB.screenMax.x, pointInScreen.x), max(rayPacketAABB.screenMax.y, pointInScreen.y));
-// }
-
-// // workgroupBarrier();
-// }
-
 fn _isAABBIntersection(aabb1:AABB2D, aabb2:AABB2D) ->bool {
 	if(aabb2.screenMax.x < aabb1.screenMin.x || aabb2.screenMin.x > aabb1.screenMax.x ||
 		aabb2.screenMax.y < aabb1.screenMin.y || aabb2.screenMin.y > aabb1.screenMax.y ){
@@ -205,20 +178,6 @@ fn _isRayPacketAABBIntersectWithTopLevelNode(aabb:AABB2D, node:TopLevel) ->bool 
 }
 
 fn _findFirstActiveRayIndex(firstActiveRayIndex:u32,pointInScreen: vec2<f32>, LocalInvocationIndex : u32, node: TopLevel) -> u32 {
-    // if(LocalInvocationIndex >= firstActiveRayIndex){
-    //     rayPacketTempForFindFirstActiveRayIndex[LocalInvocationIndex - firstActiveRayIndex] = _isPointIntersectWithTopLevelNode(pointInScreen, node);
-    // }
-
-    // workgroupBarrier();
-
-
-
-  //  if(LocalInvocationIndex == 0){
-  //   if(firstActiveRayIndex == 0){
-  //         rayPacketTempForFindFirstActiveRayIndex[0] = _isPointIntersectWithTopLevelNode(pointInScreen, node);
-  //   }
-  // }
-
     if(LocalInvocationIndex == 0){
         var result:u32 = 100;
         for (var s: u32 = 0; s < 64; s +=1) {
@@ -234,9 +193,7 @@ rayPacketTemp2ForFindFirstActiveRayIndex = result;
 
     workgroupBarrier();
 
-//     // return result + firstActiveRayIndex;
     return rayPacketTemp2ForFindFirstActiveRayIndex + firstActiveRayIndex;
-    // return  firstActiveRayIndex;
 }
 
 fn _getPositiveInfinity()->f32{
@@ -277,17 +234,11 @@ fn _getMultiplierForBuildRayPacketAABB(firstActiveRayIndex:u32) -> f32{
   return 7.0;
 }
 
-// fn _convertStartFromLeftBottomToLeftTop(LocalInvocationIndex : u32) -> u32 {
-
-// }
-
 fn _intersectScene(ray: Ray, LocalInvocationIndex : u32) -> RingIntersect {
   var intersectResult: RingIntersect;
 
   intersectResult.isClosestHit = false;
   intersectResult.layer = 0;
-
-// rayPacketRingIntersects[LocalInvocationIndex] = intersectResult;
 
 var rootNode = topLevel.topLevels[0];
 
@@ -319,24 +270,11 @@ var isFirstActiveRayIndexsChange = true;
 while(localStackSize > 0){
         rayPacketTempForFindFirstActiveRayIndex[LocalInvocationIndex] = false;
 
-//         if(LocalInvocationIndex == 0){
-//             // stackSize = localStackSize;
-
-
-// // rayPacketAABB.screenMin = vec2<f32>(_getPositiveInfinity(), _getPositiveInfinity());
-// // rayPacketAABB.screenMax = vec2<f32>(_getNegativeInfinity(), _getNegativeInfinity());
-
-
-// isRayPacketAABBIntersectWithTopLevelNode = false;
-//         }
-
         workgroupBarrier();
 
 
-        // localStackSize = stackSize;
 
 		let currentNode = stackContainer[localStackSize - 1];
-		// stackSize = stackSize - 1;
         localStackSize -= 1;
 
 		var leafInstanceCountAndMaxLayer = u32(currentNode.leafInstanceCountAndMaxLayer);
@@ -354,104 +292,6 @@ rayPacketRingIntersectLayer[LocalInvocationIndex] = 0;
         }
 
 workgroupBarrier();
-
-//paralle reduction to find min
-
-// for (var s: u32 = 1; s < 64; s = s * 2) {
-//   var index = 2 * s * LocalInvocationIndex;
-//   if(index < 64){
-//     rayPacketRingIntersectLayer[index] = min(rayPacketRingIntersectLayer[index], rayPacketRingIntersectLayer[index + s]);
-//   }
-//   workgroupBarrier();
-// }
-
-
-//     if(LocalInvocationIndex == 0){
-//         var result = false;
-//         for (var s: u32 = firstActiveRayIndex; s < 64; s +=1) {
-//             if(rayPacketRingIntersectLayer[s] == 0){
-//                 result = true;
-//                 break;
-//             }
-//         }
-
-// rayPacketHasNotIntersectRay = result;
-//     }
-
-// workgroupBarrier();
-
-// if(!rayPacketHasNotIntersectRay){
-// for (var s: u32 = firstActiveRayIndex; s < 64; s +=1) {
-//   var index = 2 * s * LocalInvocationIndex;
-//   if(index < 64){
-//     rayPacketRingIntersectLayer[index] = min(rayPacketRingIntersectLayer[index], rayPacketRingIntersectLayer[index + s]);
-//   }
-//   workgroupBarrier();
-// }
-
-
-
-//     if(LocalInvocationIndex == 0){
-//         var result:u32 = 0;
-//         for (var s: u32 = firstActiveRayIndex; s < 64; s +=1) {
-//           var layer = rayPacketRingIntersectLayer[s];
-//             if(layer == 0){
-//                 result = 0;
-//                 break;
-//             }
-
-//           result = min(result, layer);
-// //           if(result > rayPacketRingIntersectLayer[s]){
-// // result = rayPacketRingIntersectLayer[s];
-// //           }
-//         }
-
-// rayPacketRingIntersectMinLayer = result;
-//     }
-
-// workgroupBarrier();
-
-// if(maxLayer <= rayPacketRingIntersectMinLayer){
-//    continue;
-//  }
-
-
-
-
-// for (var s: u32 = 1; s < 64; s = s * 2) {
-//   if(LocalInvocationIndex % (2 * s) == 0){
-//     rayPacketRingIntersectLayer[LocalInvocationIndex] = min(rayPacketRingIntersectLayer[LocalInvocationIndex], rayPacketRingIntersectLayer[LocalInvocationIndex + s]);
-//   }
-
-//   workgroupBarrier();
-// }
-
-
-
-
-// for (var s: u32 = 1; s < 64; s = s * 2) {
-//   var index = 2 * s * LocalInvocationIndex;
-//   if(index < 64){
-//     rayPacketRingIntersectLayer[index] = min(rayPacketRingIntersectLayer[index], rayPacketRingIntersectLayer[index + s]);
-//   }
-
-//   workgroupBarrier();
-// }
-
-
-
-
-// for (var s: u32 = 32; s > 0; s >>= 1) {
-//   if(LocalInvocationIndex < s){
-//     rayPacketRingIntersectLayer[LocalInvocationIndex] = min(rayPacketRingIntersectLayer[LocalInvocationIndex], rayPacketRingIntersectLayer[LocalInvocationIndex + s]);
-//   }
-
-//   workgroupBarrier();
-// }
-
-
-
-
 
 
       if (LocalInvocationIndex < 32){
@@ -485,58 +325,10 @@ if(isNodeBehindRayPacket){
 
 
 
-
-
-
-        // _buildRayPacketAABB(firstActiveRayIndex, pointInScreen, LocalInvocationIndex);
-
-// rayPacketPointInScreenForFindMin[LocalInvocationIndex] = pointInScreen;
-// rayPacketPointInScreenForFindMax[LocalInvocationIndex] = pointInScreen;
-
-// workgroupBarrier();
-
-// //TODO perf: unroll?
-
-// //paralle reduction to find min, max
-
-// for (var s: u32 = 1; s < 64; s = s * 2) {
-//   var index = 2 * s * LocalInvocationIndex;
-//   if(LocalInvocationIndex % (2 * s) == 0){
-//     rayPacketPointInScreenForFindMin[LocalInvocationIndex] = min(rayPacketPointInScreenForFindMin[LocalInvocationIndex], rayPacketPointInScreenForFindMin[LocalInvocationIndex + s]);
-//     rayPacketPointInScreenForFindMax[LocalInvocationIndex] = max(rayPacketPointInScreenForFindMax[LocalInvocationIndex], rayPacketPointInScreenForFindMax[LocalInvocationIndex + s]);
-//   }
-//   workgroupBarrier();
-// }
-
-
-
-
-//         if(LocalInvocationIndex == 0){
-// // var rayPacketAABB: AABB2D;
-// // rayPacketAABB.screenMin = rayPacketPointInScreenForFindMin[0];
-// // rayPacketAABB.screenMax = rayPacketPointInScreenForFindMax[0];
-
-
-
-
-
-// isRayPacketAABBIntersectWithTopLevelNode = _isRayPacketAABBIntersectWithTopLevelNode(rayPacketAABB, currentNode);
-//         }
-
-//         workgroupBarrier();
-
-// 		if (!isRayPacketAABBIntersectWithTopLevelNode) {
-// 			continue;
-// 		}
-
-
     var isPointIntersectWithTopLevelNode:bool;
 
 if(LocalInvocationIndex == 0){
   //pointInScreen is left-bottom conner point of 8*8 region
-
-
-  // var rayPacketAABB:AABB2D;
 
 
   if(isFirstActiveRayIndexsChange){
@@ -570,15 +362,6 @@ else if(LocalInvocationIndex >= firstActiveRayIndex){
       workgroupBarrier();
     }
 
-//     var isPointIntersectWithTopLevelNode:bool;
-
-//     if(LocalInvocationIndex >= firstActiveRayIndex){
-// isPointIntersectWithTopLevelNode = _isPointIntersectWithTopLevelNode(pointInScreen, currentNode);
-//         rayPacketTempForFindFirstActiveRayIndex[LocalInvocationIndex - firstActiveRayIndex] = isPointIntersectWithTopLevelNode;
-//     }
-
-    // workgroupBarrier();
-
     var firstActiveRayIndexBefore = firstActiveRayIndex;
 
         firstActiveRayIndex = _findFirstActiveRayIndex(firstActiveRayIndex,pointInScreen, LocalInvocationIndex , currentNode);
@@ -603,8 +386,6 @@ isFirstActiveRayIndexsChange = false;
         if (_isLeafNode(leafInstanceCount)) {
             if(LocalInvocationIndex >= firstActiveRayIndex && isPointIntersectWithTopLevelNode){
                 var leafInstanceOffset = u32(currentNode.leafInstanceOffset);
-
-                // var intersectResult = rayPacketRingIntersects[LocalInvocationIndex];
 
                 var isBreak =false;
                 while(leafInstanceCount > 0){
